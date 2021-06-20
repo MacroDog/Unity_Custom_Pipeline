f4bf63232e76d1d8bcf7e0f3156924ce6a2eefc6
yfxie <yifeng_email@126.com>
2021-06-20 14:13:54 +0800

    Init

---
+++ Assets/Pipeline/Shaders/Unlit.shader
@@ -0,0 +1,24 @@
+﻿Shader "SRP/Unlit"
+{
+    Properties
+    {
+        _Color ("Color",Color)=(1,1,1,1)
+        // _MainTex ("Texture", 2D) = "white" {}
+    }
+    SubShader
+    {
+        Pass
+        {
+            // Tags{"LightMode" = "SRPDefaultUnlit"}
+            HLSLPROGRAM
+            #pragma target 3.5
+            #pragma multi_compile_instancing
+			#pragma instancing_options assumeuniformscaling
+            #pragma prefer_hlslcc gles
+            #pragma vertex UnlitPassVertex
+			#pragma fragment UnlitPassFragment
+			#include "../ShaderLibrary/Unlit.hlsl"
+            ENDHLSL
+        }
+    }
+}
---
+++ Assets/Scripts/InstanceColor.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstanceColor : MonoBehaviour
+{
+    static MaterialPropertyBlock propertyBlock;
+	static int colorID = Shader.PropertyToID("_Color");
+    [SerializeField]
+    Color color;
+    // Start is called before the first frame update
+
+    void OnValidate()
+    {
+        if (propertyBlock == null)
+        {
+            propertyBlock = new MaterialPropertyBlock();
+        }
+        propertyBlock.SetColor(colorID, color);
+        GetComponent<MeshRenderer>().SetPropertyBlock(propertyBlock);
+    }
+    void Awake()
+    {
+        OnValidate();
+    }
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
